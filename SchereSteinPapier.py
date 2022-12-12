4c4a9fbec6df0b936ad2d707a3c8f9b35d5997f6
shortguido <planesshorty@gmail.com>
2022-12-12 18:10:10 +0100

    Game working

--- SchereSteinPapier.py
+++ SchereSteinPapier.py
@@ -1,40 +1,40 @@
 import random
-eingabe = str(input("Schere, Stein, Papier, Echse oder Spock?"))
-pc = random.randint(1,5)
+pc = random.choice(["Stein","Schere","Papier","Echse","Spock"])
 stats = {"Stein": 0, "Papier": 0, "Schere": 0, "Echse": 0, "Spock": 0}
 menschanalyse = {"Stein": 0, "Papier": 0, "Schere": 0, "Echse": 0, "Spock": 0}
 win = {"PC": 0, "Mensch": 0, "U": 0}
 
-def checkwinner(pc, e):
-    if pc == 1:
-        pc = "Schere"
-    elif pc == 2:
-        pc == "Stein"
-    elif pc == 3:
-        pc == "Papier"
-    elif pc == 4:
-        pc == "Echse"
-    elif pc == 5:
-        pc == "Spock"
-
-    #Check for winner
-    if e == pc:
-        print("Draw")
-        stats["U"] += 1
-        return
+def destroy(e):
     if e == "Schere":
-        menschanalyse["Schere"] += 1
-        if pc == "Papier":
-            print("Schere schneidet Papier")
-            stats["Schere"] += 1
-            return
-        elif pc == "Stein":
-            print("Stein schlägt Schere")
-            stats["Stein"] += 1
-        elif pc == "Vollkatastrophe":
-            pass
+        return "Papier","Echse"
+    elif e == "Papier":
+        return "Stein", "Spock"
+    elif e == "Stein":
+        return "Echse", "Schere"
+    elif e == "Echse":
+        return "Spock", "Papier"
+    elif e == "Spock":
+        return "Stein", "Schere"
+
+def game():
+    eingabe = str(input("Schere, Stein, Papier, Echse oder Spock? "))
+    if pc in destroy(eingabe):
+        print("PC hat gewählt: " + pc)
+        print("Du hast gewonnen!")
+    elif pc == eingabe:
+        print("PC hat gewählt: " + pc)
+        print("Gleiche Wahl!")
+    else:
+        print("PC hat gewählt: " + pc)
+        print("Du hast verloren!")
+
+    jn = str(input("Weiterspielen? [j/n]: "))
+    if jn == "j":
+        game()
+    else:
+        exit()
 
 
 
 if __name__ == "__main__":
-    pass
+    game()
